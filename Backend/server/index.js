8a0aefe8f7b641ee08eadf3dbb6f2e9c23f31390
theoneste1 <theoneste99@gmail.com>
2020-08-11 06:56:26 +0200

    I can delete, create, update an article

--- Backend/server/index.js
+++ Backend/server/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 // importing mongoose package
 const mongoose = require("mongoose")
 
+const blogController=require("../controller/blogs")
 //connect mongoose to database
 mongoose
     .connect("mongodb://localhost:27017/theoneste", { useNewUrlParser: true })
@@ -13,9 +14,14 @@ mongoose
         // this variable app let us doeverything we need to configure our ReSP API,
         //like registering our routers, installing necessary middlewares
         const app = express();
-        app.get('/', (req, res) => {
-            res.send("updated!");
-        });
+        app.use(express.json());
+        app.get('/blogs', blogController.findBlogs);
+        app.get("/blogs/:id", blogController.findBlog);
+        app.post("/blogs", blogController.createBlog);
+        app.patch("/blogs/:id", blogController.updateBlog);
+        app.delete("/blogs/:id", blogController.deleteBlog);
+
+
 
         app.listen(8000, () => {
             console.log("Server has started  at port 8000")
